e45a61fa29f87df8dc1c83f644ea83b4192e42d2
Cláudio Aguiar <claudioaguiargomes@gmail.com>
2022-07-23 21:13:21 -0400

    Update contador.py
    Diminuição das linhas de código, melhorias nas repetições e aperfeiçoamento do mecanismo, com a possibilidade do usuario selecionar quantas e quais notas ele quer para compor a contagem.

--- contador.py
+++ contador.py
@@ -1,48 +1,17 @@
-print('='*40)
-print('{:^40}'.format('Contador de Cédulas'))
-print('='*40)
-valor = int(input('Digite o valor a ser sacado: '))
-nota50 = nota20 = nota10 = nota01 = 0
-while valor >= 50:
-    valor -= 50
-    nota50 += 1
-if nota50 != 0:
-    print(f'Total de {nota50} cédulas de R$50')
-while valor >= 20:
-    valor -= 20
-    nota20 += 1
-if nota20 != 0:
-    print(f'Total de {nota20} cédulas de R$20')
-while valor >= 10:
-    valor -= 10
-    nota10 += 1
-if nota10 != 0:
-    print(f'Total de {nota10} cédulas de R$10')
-while valor >= 1:
-    valor -= 1
-    nota01 += 1
-if nota01 != 0:
-    print(f'Total de {nota01} cédulas de R$1')
+ced = []
+ced2 = [0]
+tip = int(input('Quantos tipos de cédulas terão?'))
 
-'''
-r = (int(input('Cédula número um: ')),int(input('Cédula número dois: ')),int(input('Cédula número três: ')))
+for cont in range(1,tip+1):
+    ced.append(int(input(f'Digite o valor da {cont}º cédula: ')))
+ced.sort(reverse=True)
+
+for c in range (1,tip+1):
+    ced2.append(0)
 valor = int(input('Valor a ser sacado: '))
-c1 = c2 = c3 = 0
-
-while valor >= r[0]:
-    valor -= r[0]
-    c1 += 1
-if c1 != 0:
-    print(f'Total de {c1} cédulas de R${r[0]}')
-while valor >= r[1]:
-    valor -= r[1]
-    c2 += 1
-if c2 != 0:
-    print(f'Total de {c2} cédulas de R${r[1]}')
-while valor >= r[2]:
-    valor -= r[2]
-    c3 += 1
-if c3 != 0:
-    print(f'Total de {c3} cédulas de R${r[2]}')
-    
-'''
+for z in range(0,tip):
+    while valor >= ced[z]:
+        valor -= ced[z]
+        ced2[z] += 1
+    if ced2[z] != 0:
+        print(f'Total de {ced2[z]} cédulas de R${ced[z]}')
